ca82b5c233bffd42194b4af5d51f5812f7266dc4
RCVictor <victorz.ramosc@gmail.com>
2023-04-25 20:30:32 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './App.scss';
 function App() {
 
   return (
-    <article role='presentation' className='card'>
+    <aside role='article' className='card'>
       <img src="/assets/qr-code.png" alt="QR Code" />
       <h1>Improve your front-end  skills by building projects</h1>
       <p>
         Scan the QR to visit Frontend Mentor and take your coding skills to the next level
       </p>
-    </article>
+    </aside>
   )
 }
 
